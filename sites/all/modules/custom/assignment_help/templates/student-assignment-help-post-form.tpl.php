90e8e0c7e0676a933fc3575eb903a5d5153a7506
Rajnish Rao <rajnishrao10@gmail.com>
2018-11-18 11:52:59 +0530

    main_banner content added

--- sites/all/modules/custom/assignment_help/templates/student-assignment-help-post-form.tpl.php
+++ sites/all/modules/custom/assignment_help/templates/student-assignment-help-post-form.tpl.php
@@ -1,20 +1,52 @@
-<div class="student-assignement-post-form-wrapper">
+<div class="main-banner">
     <div class="row">
-        <div class="col-sm-2">
+    <div class="col-md-7 col-md-offset-5  col-sm-10 col-sm-offset-1">
+    <div class="form-container">
+        <h1>ASSIGNMENT HELP</h1>
+        <h2>Get Assignment Help From World's No.1 Assignment Help Company</h2>
+        <div class="row">
+        <div class="col-sm-6">
             <?php print drupal_render($form['mail']); ?>
         </div>
-        <div class="col-sm-2">
+        <div class="col-sm-6">
             <?php print drupal_render($form['subject']); ?>
         </div>
-
-
-        <div class="col-sm-2">
+        <div class="col-sm-6">
             <?php print drupal_render($form['no_of_pages']); ?>
         </div>
-        <div class="col-sm-2">
+        <div class="col-sm-6">
             <?php print drupal_render($form['date']); ?>
         </div>
+        </div>
+        <div class="text-center form-actions">
+    	<?php print drupal_render_children($form); ?>
+        </div>
     </div>
-    <?php print drupal_render_children($form); ?>
+    </div>
+    </div>
+	<!-- Banner text-->    
+    <div class="row main-banner-content">
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <ul class="list-unstyled">
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> Assignment Help </li>
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> Highest Grade for All Academic Needs</li>
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> 100+ Students Support Executive to Listen Students Requirement</li>
+        </ul>    
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <ul class="list-unstyled">
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i>Global Assignment Helper</li>
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> Over 200k Satisfied Students</li>
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> 10+ Year Of Experience in Academic Writing</li>
+            </ul>
+         </div>    
+        <div class="col-md-4 col-sm-4 col-xs-12">
+            <ul class="list-unstyled">
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> Best Assignment Writer</li>
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> 4500+ Academic Assignment Writers</li>
+            <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> Round the Clock Support Service</li>
+        </ul>
+        </div>
+    </div>
 </div>
 
